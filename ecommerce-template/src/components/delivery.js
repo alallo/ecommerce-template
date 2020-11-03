cbd5475e5a09204b0e85cb57b758eb2d2c0dbd7b
unknown <alessandro.lallo@gmail.com>
2020-11-03 13:52:00 +0000

    Initial commit

---
+++ ecommerce-template/src/components/delivery.js
@@ -0,0 +1,20 @@
+
+
+function Delivery() {
+    return (
+            <section class="bg-white py-8">
+
+                <div class="container py-8 px-6 mx-auto">
+
+                    <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl mb-8" href="#">
+                    Delivery
+                    </a>
+
+                    <p class="mt-8 mb-8">This part is for the delivery costs</p>
+                </div>
+            </section>
+    );
+  }
+  
+  export default Delivery;
+  
