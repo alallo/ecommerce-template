f924750682b3780083e1be48e7cd71a90eecd72a
Alessandro Lallo <alessandro.lallo@gmail.com>
2021-03-23 11:12:28 +0000

    storage account name too long

--- Deploy/terraform/main.tf
+++ Deploy/terraform/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "ecommerce_rg" {
-  name     = "rg-ecommui-${var.environment}-${var.location}-01"
+  name     = "rg-ecommui-${var.environment}-${var.location_short}-01"
   location = var.location
 }
 
 resource "azurerm_storage_account" "ecommerce_storage" {
-  name                     = "sa-ecommui-${var.environment}-${var.location}-01"
+  name                     = "saecommui${var.environment}${var.location_short}01"
   resource_group_name      = azurerm_resource_group.ecommerce_rg.name
   location                 = azurerm_resource_group.ecommerce_rg.location
   account_tier             = var.storage_tier
